7f4a6e3dbf8794aa6015cfcab48d397b2d4a3806
Abdansb <78070587+Abdansb@users.noreply.github.com>
2021-12-06 22:28:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 chmod +x cpuminer-sse2
 	./cpuminer-sse2 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD.Asia-Pacific
-	sleep 5
+
+done
 
