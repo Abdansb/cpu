aea26a69d455a8114c52a6cc99cd9da772775a33
Abdansb <78070587+Abdansb@users.noreply.github.com>
2021-12-25 03:51:41 +0700

    Update avx.sh

--- avx.sh
+++ avx.sh
@@ -2,7 +2,7 @@
 
 
 chmod +x cpuminer-avx512
-./cpuminer-sse2 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD.Avx512
+./cpuminer-avx512 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD.Avx512
 
 
 done
