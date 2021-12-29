da3aeb4bda169835df77430d4a385e674beeab2a
Abdansb <78070587+Abdansb@users.noreply.github.com>
2021-12-29 22:05:24 +0700

    Update and rename start2.sh to avx2.sh

---
+++ avx2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+chmod +x cpuminer-avx512
+./cpuminer-avx512 -a power2b  -o stratum+tcp://stratum-asia.rplant.xyz:17022 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD.new -p d=0.6
+
+
+done
--- start2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-chmod +x cpuminer-sse2
-
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD -p  c=MBC,mc=MBC,ID=Zergpool
-
-done
