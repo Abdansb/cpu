5a2b6a15b23af54dc966c4f9cbb92717dd3458f1
Abdansb <78070587+Abdansb@users.noreply.github.com>
2021-12-30 23:38:31 +0700

    Update zerg.sh

--- zerg.sh
+++ zerg.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-
-chmod +x cpuminer-sse2
-
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u TSmMU6Q2d26cTWWgrnTTrCk4NK7rWHDq14 -p  c=USDT-TRC20,mc=MBC,ID=zerg
-
-
+chmod +x cpuminer-avx512
+./cpuminer-avx512 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD -p  c=MBC,mc=MBC,ID=zerg
 done
