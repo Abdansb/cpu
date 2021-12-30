ef1fa4c79cc779b82292c0fd149c90eeac803794
Abdansb <78070587+Abdansb@users.noreply.github.com>
2021-12-30 23:22:03 +0700

    Update and rename start3.sh to zerg.sh

--- start3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-chmod +x cpuminer-sse2
-
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD.Abdan_4
-
-
-done
---
+++ zerg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+chmod +x cpuminer-sse2
+
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u TSmMU6Q2d26cTWWgrnTTrCk4NK7rWHDq14 -p  c=USDT-TRC20,mc=MBC,ID=zerg
+
+
+done
