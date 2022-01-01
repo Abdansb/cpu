39c7b73be95c2b599c569eeeddfc10139658e659
Abdansb <78070587+Abdansb@users.noreply.github.com>
2022-01-01 14:13:12 +0700

    Update avx2.sh

--- avx2.sh
+++ avx2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-
 chmod +x cpuminer-avx512
-./cpuminer-avx512 -a power2b  -o stratum+tcp://stratum-asia.rplant.xyz:17022 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD.new -p d=0.6
-
-
+./cpuminer-avx512 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MrKVWSbyNrmar1TrQiiUXMkecwvdG3x9bD.Test Config
 done
